80ad5b9853a0c2c326f045aa3c5243d6e72132a6
Kaiser Quezada <kaiser@sci5.com>
2022-03-19 12:49:37 +0800

    implement react-router, add navigation, directory component

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,26 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo} from "../../assets/crown.svg";
+
+import "./navigation.component.scss"
+
+const Navigation = () => {
+	return (
+		<Fragment>
+			<div className='navigation'>
+				<Link class='logo-container' to='/'>
+					<CrwnLogo className='logo' />
+				</Link>
+				<div className='nav-links-container'>
+					<Link className='nav-link' to='/shop'>
+						SHOP
+					</Link>
+				</div>
+			</div>
+			<Outlet/>
+		</Fragment>
+	);
+};
+
+export default Navigation;
