db99bfb051215fda493bac6a781f0646df2b222f
Kaiser Quezada <kaiser@sci5.com>
2022-05-07 23:35:16 +0800

    create checkout page, checkout items, and checkout functionality

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,9 +1,36 @@
-import 'checkout.styles.scss';
+import {useContext, useRef} from "react";
+
+import {CartContext} from "../../contexts/cart.context";
+
+import CheckoutItem from "../../component/checkout-item/checkout-item.component";
+
+import './checkout.styles.scss';
 
 const Checkout = () => {
+	const { cartItems, cartTotal } = useContext(CartContext);
 	return (
-		<div>
-			
+		<div className="checkout-container">
+			<div className="checkout-header">
+				<div className="header-block">
+					<span>Product</span>
+				</div>
+				<div className="header-block">
+					<span>Description</span>
+				</div>
+				<div className="header-block">
+					<span>Quantity</span>
+				</div>
+				<div className="header-block">
+					<span>Price</span>
+				</div>
+				<div className="header-block">
+					<span>Remove</span>
+				</div>
+			</div>
+			{cartItems.map((cartItem) => (
+				<CheckoutItem key={cartItem.id} cartItem={cartItem}/>
+			))}
+			<span className="total">Total: ${cartTotal}</span>
 		</div>
 	);
 };
